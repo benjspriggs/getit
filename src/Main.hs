c4bed622e1e2f70b1262372dfb77e35202454f64
bspriggs <ben@sprico.com>
2018-05-14 19:55:22 -0700

    Add docopt interface in main, usage

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,21 @@
+{-# LANGUAGE QuasiQuotes #-}
 import Data.DateTime
+import Control.Monad
+import System.Environment
+import System.Console.Docopt
 import Item
 
+patterns :: Docopt
+patterns = [docoptFile|src/USAGE.txt|]
+
+getArgOrExit = getArgOrExitWith patterns
+
+main :: IO()
 main = do
-	ct <- return (fromGregorian' 2019 2 3)
-	isDue <- due (Todo ct)
-	putStrLn (show ct)
-	putStrLn (show isDue)
-	putStrLn "Hello world!"
+  args <- parseArgsOrExit patterns =<< getArgs
+  when (args `isPresent` (command "new")) $ do
+    putStrLn "new"
+  when (args `isPresent` (command "list")) $ do
+    putStrLn "list"
+  when (args `isPresent` (command "done")) $ do
+    putStrLn "done"
