1c95be67b9997cfdb5714e00942530c6866ab839
bspriggs <ben@sprico.com>
2018-05-30 18:39:10 -0700

    Add sketch for solver

---
+++ src/Solver.hs
@@ -0,0 +1,47 @@
+module Solver where
+import Data.List(permutations, concat, sortBy)
+import Data.Time(UTCTime)
+import Data.Maybe(Maybe, fromMaybe)
+import Item(TodoItem)
+
+data OpenConstraint a = Open a (Maybe UTCTime) (Maybe UTCTime)
+data SolvedConstraint a = Solved a UTCTime UTCTime
+type Solution a = ([SolvedConstraint a], [OpenConstraint a])
+
+valid :: Solution a -> Bool
+valid = error "todo"
+
+solveSchedule :: UTCTime -> UTCTime -> [OpenConstraint a] -> [Solution a]
+solveSchedule start end constraints = [ solution | solution <- possibleSolutions start end constraints, valid solution ]
+
+subsets       :: [a] -> [[a]]
+subsets []     = [[]]
+subsets (x:xs) = subsets xs ++ map (x:) (subsets xs)
+
+-- Sorts OpenConstraint.
+sortPredicate :: OpenConstraint a -> OpenConstraint a -> Ordering
+sortPredicate (Open _ mStartA _) (Open _ mStartB _) = case (mStartA, mStartB) of
+  (Nothing, Nothing) -> EQ
+  (Nothing, _) -> LT
+  (_, Nothing) -> GT
+  (Just a, Just b) -> compare a b
+
+-- Gives a time to an open constraint.
+-- Defaults to the first two provided dates.
+giveTime :: UTCTime -> UTCTime -> OpenConstraint a -> SolvedConstraint a
+giveTime s e (Open item mStart mEnd) = Solved item newStart newEnd
+  where newStart = fromMaybe s mStart
+        newEnd = fromMaybe e mEnd
+
+allPossibleSplits :: [OpenConstraint a] -> [([OpenConstraint a], [OpenConstraint a])]
+allPossibleSplits cs = [splitAt n cs | n <- [0..(length cs)]]
+
+asSolution :: UTCTime -> UTCTime -> ([OpenConstraint a], [OpenConstraint a]) -> Solution a
+asSolution s e (toSolve, toOpen) = (map (giveTime s e) toSolve, toOpen)
+
+-- Given a start and ending window, and a list of 
+-- OpenConstraint, return a list of solution sets
+-- that may or may not be valid.
+possibleSolutions :: UTCTime -> UTCTime -> [OpenConstraint a] -> [Solution a]
+possibleSolutions start end constraints = [ asSolution start end possible | possible <- allPossibleSplits constraints ]
+  where sortedConstraints = sortBy sortPredicate constraints
